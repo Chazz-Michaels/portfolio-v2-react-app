b5cad709f3770f0070018800d33e87a80e59a06d
Chazz Michaels <michaelchasedev@gmail.com>
2018-11-15 19:32:58 -0500

    add content and animations to menu, add particles js in welcome component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavigationMenu from './components/navigationMenu';
+import Welcome from './components/welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
         <NavigationMenu />
+        <Welcome />
       </div>
     );
   }
--- src/components/navigationMenu.jsx
+++ src/components/navigationMenu.jsx
@@ -3,35 +3,91 @@ import React, { Component } from 'react';
 
 class NavigationMenu extends Component {
 
+  mouseEnterNavMenu = () => {
+    const u1 = document.getElementById('ul-1');
+    const u2 = document.getElementById('ul-2');
+    const u3 = document.getElementById('ul-3');
+    const u4 = document.getElementById('ul-4');
+    const u5 = document.getElementById('ul-5');
+    const faHome = document.getElementById('i1');
+    const faGraduationCap = document.getElementById('i2');
+    const faHandshake = document.getElementById('i3');
+    const faBolt = document.getElementById('i4');
+    const faEnvelope = document.getElementById('i5');
+    u1.style.marginLeft = 0;
+    u2.style.marginLeft = 0;
+    u3.style.marginLeft = 0;
+    u4.style.marginLeft = 0;
+    u5.style.marginLeft = 0;
+    faHome.style.color = '#ff6347';
+    faGraduationCap.style.color = 'var(--blue)';
+    faHandshake.style.color = 'var(--green)';
+    faBolt.style.color = 'yellow';
+    faEnvelope.style.color = 'var(--purple)';
+  }
+  mouseLeaveNavMenu = () => {
+    const u1 = document.getElementById('ul-1');
+    const u2 = document.getElementById('ul-2');
+    const u3 = document.getElementById('ul-3');
+    const u4 = document.getElementById('ul-4');
+    const u5 = document.getElementById('ul-5');
+    const faHome = document.getElementById('i1');
+    const faGraduationCap = document.getElementById('i2');
+    const faHandshake = document.getElementById('i3');
+    const faBolt = document.getElementById('i4');
+    const faEnvelope = document.getElementById('i5');
+    u1.style.marginLeft = "-100px";
+    u2.style.marginLeft = "-100px";
+    u3.style.marginLeft = "-100px";
+    u4.style.marginLeft = "-100px";
+    u5.style.marginLeft = "-100px";
+    faHome.style.color = '#FFFFFF';
+    faGraduationCap.style.color = '#FFFFFF';
+    faHandshake.style.color = '#FFFFFF';
+    faBolt.style.color = '#FFFFFF';
+    faEnvelope.style.color = '#FFFFFF';
+  }
+
   render () {
     return (
-      <div id="nav-menu-wrapper">
+      <div onMouseEnter={this.mouseEnterNavMenu} onMouseLeave={this.mouseLeaveNavMenu} id="nav-menu-wrapper">
         <div id="menu-items-wrapper">
+
           <div className="menu-item">
-            <i className="fa fa-home" aria-hidden="true"></i>
+            <i className="fa fa-home" id="i1" aria-hidden="true"></i>
             <br />
             <span className="menu-item-text">Home</span>
+            <span className="nav-underline" id="ul-1"></span>
           </div>
+
           <div className="menu-item">
-            <i className="fa fa-graduation-cap" aria-hidden="true"></i>
+            <i className="fa fa-graduation-cap" id="i2" aria-hidden="true"></i>
             <br />
             <span className="menu-item-text">About Me</span>
+            <span className="nav-underline" id="ul-2"></span>
           </div>
+
           <div className="menu-item">
-            <i className="fa fa-handshake" aria-hidden="true"></i>
+            <i className="fa fa-handshake" id="i3" aria-hidden="true"></i>
             <br />
             <span className="menu-item-text">Projects</span>
+            <span className="nav-underline" id="ul-3"></span>
           </div>
+
           <div className="menu-item">
-            <i className="fa fa-bolt" aria-hidden="true"></i>
+            <i className="fa fa-bolt" id="i4" aria-hidden="true"></i>
             <br />
             <span className="menu-item-text">Resources</span>
+            <span className="nav-underline" id="ul-4"></span>
           </div>
+
           <div className="menu-item">
-            <i className="fa fa-envelope" aria-hidden="true"></i>
+            <i className="fa fa-envelope" id="i5" aria-hidden="true"></i>
             <br />
             <span className="menu-item-text">Contact Me</span>
+            <span className="nav-underline" id="ul-5"></span>
           </div>
+
         </div>
       </div>
     );
